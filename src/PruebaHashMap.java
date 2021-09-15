6435553d9632b237a9c602b51d2a542205397c99
Armando Enriquez <armandoenriquez0531@gmail.com>
2021-09-15 14:31:54 -0500

    Se creó la clase Alumno

---
+++ src/PruebaHashMap.java
@@ -0,0 +1,55 @@
+class Alumno{
+	private String nombre;
+	private int edad;
+	private String carrera;
+	private String fechaInscripcion;
+	
+	public Alumno(String nombre, int edad, String carrera, String fechaInscripcion) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		this.carrera = carrera;
+		this.fechaInscripcion = fechaInscripcion;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getCarrera() {
+		return carrera;
+	}
+	public void setCarrera(String carrera) {
+		this.carrera = carrera;
+	}
+	public String getFechaInscripcion() {
+		return fechaInscripcion;
+	}
+	public void setFechaInscripcion(String fechaInscripcion) {
+		this.fechaInscripcion = fechaInscripcion;
+	}
+
+	@Override
+	public String toString() {
+		return "Alumno [nombre=" + nombre + ", edad=" + edad + ", carrera=" + carrera + ", fechaInscripcion="
+				+ fechaInscripcion + "]";
+	}
+	
+	
+}
+public class PruebaHashMap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
